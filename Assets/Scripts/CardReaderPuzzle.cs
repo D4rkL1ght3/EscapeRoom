2f0c290c7b7a8445f764b2a13a7e75d7e8cf309d
D4rkL1ght3 <farrellimpirson@gmail.com>
2026-05-15 00:51:46 +0700

    Added Keycard puzzle script

---
+++ Assets/Scripts/CardReaderPuzzle.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using TMPro;
+
+
+public class CardReaderPuzzle : MonoBehaviour
+{
+    [Header("UI")]
+    public TMP_Text hintText;
+
+    public GameObject keypadUI;
+
+    [Header("Player")]
+    public MonoBehaviour playerController;
+}
